8f7bfaa607c643a5eeaca8fdb00b8d240374548f
Yamako <isep.auchanvn@gmail.com>
2021-03-23 17:54:09 +0000

    Delete Main.java

--- Eclipse/game/src/tours/Main.java
+++ Eclipse/game/src/tours/Main.java
@@ -1,19 +1,1 @@
-package tours;
 
-public class Main {
-	public static void main(String[] args) {
-		tour();
-	}
-
-
-	public static void tour() {
-		for (int t = 0 ; t==8 ; t++) {
-			System.out.println(t);
-			if (t%2 == 0) {
-				System.out.println("Tour numero "+ t +"\n Mr.Jack Commence");
-			} else {
-				System.out.println("Tour numero "+ t + "\n L'enqueteur Commence");
-			}
-		}
-	}
-}
