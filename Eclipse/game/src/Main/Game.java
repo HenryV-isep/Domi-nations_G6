006349a68bdc8928630ee72283d7d27adb2b4c78
Cécile <74317560+Cecile-L@users.noreply.github.com>
2021-05-15 15:28:03 +0200

    p

--- Eclipse/game/src/Main/Game.java
+++ Eclipse/game/src/Main/Game.java
@@ -1,29 +1,54 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Game {
+
+	private Player[] players;
+	public int nbTuiles;
+	private Scanner clavier = new Scanner(System.in);
+
 	private void createPlayer() {
-		
+		this.players = new Player[4]; // changer pour 2 et if more players players.add
+		this.players[0] = new Player(this.clavier.nextLine(),blue);  // Ã  remplacer par la fonction qui donne la couleur
+		this.players[1] = new Player(this.clavier.nextLine(),green);
+		this.players[2] = new Player(this.clavier.nextLine(),orange);
+		this.players[3] = new Player(this.clavier.nextLine(),brown);
+		currentPlayer = this.players[0];
 	}
 	private boolean isValidMove() {
 		return true;
 	}
+
 	private void initializeGame() {
-		
+		if (this.players.length ==2){
+			nbTuiles = 24;
+
+		}
+		if else (this.players.length ==3){
+			nbTuiles = 36;
+		}
+		else (this.players.length ==4){
+			nbTuiles = 48;
+		}
 	}
+
 	private void initializeBoard() {
 		
 	}
+
+
 	
 	// On decrit brievement la logique du jeu
 	public void play() {
 	   while (true) {
-	        // attribution des rois, châteaux et connaissance du                  nombre de joueurs.
+	        // attribution des rois, chÃ¢teaux et connaissance du nombre de joueurs.
 	       createPlayers();
 
 	       // initialiser le nombre de tuiles suivant le nombre de joueurs ainsi que la liste de couleurs    
 	       initializeGame();
 
-	       // on placera le château et on choisira la couleur qui commence grâce à une liste de couleur qui donnera random l'ordre  
+	       // on placera le chÃ¢teau et on choisira la couleur qui commence grÃ¢ce Ã  une liste de couleur qui donnera random l'ordre
 	       initializeBoard();  
 	       
 	       // A voir si on choisi un while ou un for
@@ -33,14 +58,14 @@ public class Game {
 	                pioche();
 	 
 	                if (tour==0) {
-	                    // choix des dominos en fonction de l’ordre des couleurs
+	                    // choix des dominos en fonction de lï¿½ordre des couleurs
 	                    choix1();    
 	                } else {
-	                    // choix des dominos en fonction de l’ordre des dominos
+	                    // choix des dominos en fonction de lï¿½ordre des dominos
 	                    choixDomino();   
 	               }
 
-	               printBoard();       // En a t’on besoin ?? 
+	               printBoard();       // En a tï¿½on besoin ?? 
 	        
 	               String move;
 	               do {
