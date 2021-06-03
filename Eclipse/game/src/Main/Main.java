3f0e07d64f782a1164ba6ce7a2f749cd552713a1
Cécile <74317560+Cecile-L@users.noreply.github.com>
2021-06-03 15:46:05 +0200

    n

--- Eclipse/game/src/Main/Main.java
+++ Eclipse/game/src/Main/Main.java
@@ -4,27 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
-
-<<<<<<< HEAD
-public class Main {
-	public static void main(String[] args) {
-		swing interfaces = new swing();
-		Game game = new Game();
-		game.play();
-	}
-=======
 import Grap_int.swing;
 
 public class Main extends JFrame {
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(new NimbusLookAndFeel());
->>>>>>> Tendzin
-
       	SwingUtilities.invokeLater(new Runnable() {
                   @Override
                   public void run() {
                         new swing();
                   }
+                  Game game = new Game();
+                  game.play();
             });
 	}
 }
